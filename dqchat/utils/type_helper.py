52f644325832b0b1b72b88ed5c5b4e8f2f74059a
Junnos <junnos.dev@gmail.com>
2024-08-22 01:45:24 +0000

    ✨ Added `None` type exception to `guard_type`

--- dqchat/utils/type_helper.py
+++ dqchat/utils/type_helper.py
@@ -7,6 +7,11 @@ __T = TypeVar("__T")
 def guard_type(value: Any, expected_type: Type[__T]) -> __T:
     if isinstance(value, expected_type):
         return value
+    elif value is None:
+        raise TypeError(
+            f"Type mismatch: Expected solid type {expected_type.__name__}, but received 'None'"
+            f"Value: {repr(value)}"
+        )
     else:
         actual_type = type(value).__name__
         raise TypeError(
