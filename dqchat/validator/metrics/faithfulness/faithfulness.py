f8b38ab4c953c89898af6de413ccd819c6838f1d
Junnos  <junnos.dev@gmail.com>
2024-09-10 08:22:35 +0000

    ♻️ Update faithfulness to use Pipeline
    PretrainedModel generate method ➡️  Pipeline

--- dqchat/validator/metrics/faithfulness/faithfulness.py
+++ dqchat/validator/metrics/faithfulness/faithfulness.py
@@ -3,8 +3,9 @@ from typing import Optional, Union
 from datasets import Dataset
 from langchain.prompts import PromptTemplate
 from langchain_core.runnables import RunnableSequence
+from langchain_huggingface.llms import HuggingFacePipeline
 from transformers import PreTrainedModel
-from transformers.pipelines import Pipeline
+from transformers.pipelines import Pipeline, pipeline
 
 from dqchat.utils.type_helper import guard_let
 from dqchat.validator.metrics.output_parsers import JSONKeyPathOutputParser
@@ -19,23 +20,34 @@ class FaithfulnessMetric(BaseMetric):
     with the contents of your `retrieval_context`.
     """
 
-    model: Optional[PreTrainedModel] = None
+    pipeline: Optional[Union[Pipeline, HuggingFacePipeline]] = None
 
     def __init__(self, threshold: float):
         super().__init__(threshold)
+
+    def _wrap_pipeline(
+        self, pipeline: Union[Pipeline, HuggingFacePipeline]
+    ) -> HuggingFacePipeline:
+        try:
+            bare_pipe = guard_let(pipeline, Pipeline)
+            return HuggingFacePipeline(pipeline=bare_pipe)
+        except TypeError:
+            return guard_let(pipeline, HuggingFacePipeline)
 
     def _generate_truths(self) -> RunnableSequence:
         """
         Generate truths from given retrieval contexts.
         It actually returns a runnable sequence that can be invoked with key `contexts`.
         """
+        pipe = self._wrap_pipeline(self.pipeline)
+
         prompt = PromptTemplate(
             template=FaithfulnessTemplate.generate_truths(text="{contexts}"),
             input_variables=["contexts"],
         )
         chain = RunnableSequence(
             first=prompt,
-            middle=self.model,
+            middle=pipe,
             last=JSONKeyPathOutputParser(keypath="truths"),
         )
         return chain
@@ -45,13 +57,15 @@ class FaithfulnessMetric(BaseMetric):
         Generate claims from given actual outputs.
         It actually returns a runnable sequence that can be invoked with key `actual_output`.
         """
+        pipe = self._wrap_pipeline(self.pipeline)
+
         prompt = PromptTemplate(
             template=FaithfulnessTemplate.generate_claims(text="{actual_output}"),
             input_variables=["actual_output"],
         )
         chain = RunnableSequence(
             first=prompt,
-            middle=self.model,
+            middle=pipe,
             last=JSONKeyPathOutputParser(keypath="claims"),
         )
         return chain
@@ -64,6 +78,8 @@ class FaithfulnessMetric(BaseMetric):
 
         It actually returns a runnable sequence that can be invoked with keys `claims` and `truths`.
         """
+        pipe = self._wrap_pipeline(self.pipeline)
+
         prompt = PromptTemplate(
             template=FaithfulnessTemplate.generate_verdicts(
                 claims="{claims}", retrieval_context="{truths}"
@@ -73,7 +89,7 @@ class FaithfulnessMetric(BaseMetric):
         # Expected output: list of dicts with 'verdict' and Optional 'reason'
         chain = RunnableSequence(
             first=prompt,
-            middle=self.model,
+            middle=pipe,
             last=JSONKeyPathOutputParser(keypath="verdicts"),
         )
         return chain
@@ -94,17 +110,8 @@ class FaithfulnessMetric(BaseMetric):
         chain = RunnableSequence(first=prompt, last=self.model)
         return chain
 
-    def measure(self, dataset: Dataset) -> float:
-        # 1. Map dataset to LLMTestCase
-        test_case = LLMTestCase(
-            input=dataset["question"],
-            actual_output=dataset["answer"],
-            contexts=dataset["context"],
-        )
-
+    def measure(self, test_case: LLMTestCase) -> float:
         # 2. Generate truths, claims, verdicts, and reason
-        # reason = self._generate_reason() # Inference
-
         chain = RunnableSequence(
             {
                 "truths": self._generate_truths(),
@@ -114,33 +121,50 @@ class FaithfulnessMetric(BaseMetric):
             | self._calculate_score()
         )
 
-        result = chain.invoke(test_case)
+        result = chain.invoke(test_case.to_dict())
         score_result = guard_let(result, float)
         return score_result
 
     def evaluate(
-        self, dataset: Dataset, llm: Union[PreTrainedModel, Pipeline]
-    ) -> Dataset:
+        self,
+        dataset: Union[Dataset, LLMTestCase],
+        llm: Union[PreTrainedModel, Pipeline],
+    ) -> float:  # TODO: Dataset에 col 추가하여 return 하는 것으로 변경
         """
         Evaluate the faithfulness of given dataset.\\
         With given LLM using actual outputs, contexts, and pre-defined evaluation prompts,
         generate evaluation scores for each evaluation prompt.
         """
-        # 1. Prepare LLM model
+        # 1. Prepare dataset as LLMTestCase format
+        if isinstance(dataset, Dataset):
+            test_case = LLMTestCase(
+                input=dataset["question"],
+                actual_output=dataset["answer"],
+                contexts=dataset["context"],
+            )
+        elif isinstance(dataset, LLMTestCase):
+            test_case = dataset
+        del dataset
+
+        # 2. Prepare LLM model
         if isinstance(llm, PreTrainedModel):
-            model = llm
+            pipe = pipeline(
+                task="text-generation",
+                model=llm,
+                device_map="auto",
+            )
         elif isinstance(llm, Pipeline):
-            model = llm.model
-        else:
-            raise TypeError("llm must be either a PreTrainedModel or a Pipeline")
+            pipe = llm
+        del llm
 
-        model.eval()
-        self.model = model
+        pipe.model.eval()
+        self.pipeline = pipe
 
-        # 2. Calculate evaluation scores
+        # 3. Calculate evaluation scores
         # scores = []
 
-        # 2. Add column with evaluation scores
+        # 4. Add column with evaluation scores
 
-        # 3. Return dataset
-        return dataset
+        # 5. Return dataset
+        # return dataset
+        return self.measure(test_case=test_case)
